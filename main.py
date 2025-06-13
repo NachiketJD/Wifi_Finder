c7efe0d67db2d740a3e163c029d5cbb1c6fc0f8f
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-06-13 12:52:20 +0530

    Final Commit2

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import subprocess
 nw = subprocess.check_output(['netsh', 'wlan', 'show', 'network']).decode('ascii')# this object, nw, contains all the network information, including the SSID of each network, this is the name of the network this done using the netsh command in the command prompt, the wlan show network command shows all the available networks in the system, the decode function is used to convert the output to a string, and the split function is used to split the output into a list of strings, each string represents a network. This is done becoz the output of the netsh command is in bytes and we need to convert it to string to 
 print(nw)
 
+#netsh command, which is used to get the list of available networks in the system. The wlan show network command is used to get the list of available networks in the system. The output of this command is stored in the nw object.
+
 #This will print all the available networks in the system, including the SSID of each network. 
 #SSID stands for Service Set Identifier. The SSID is the name of the network, it is used to identify the network.
 #IT prints the SSID, Network Type (Infrastructure), Authentication (WPA2-Personal), Encrypytion(CCMP i.e. Counter Mode Cipher Block Chaining Message Authentication Code Protocol or CCM mode protocol which uses AES encryption || TKIP i.e. Temporal Key Integrity Protocol, provides per-packet key mixing a message integrity and re-keying mechanism).
